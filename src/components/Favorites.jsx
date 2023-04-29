4b3caecd21382e30270678a8f4d4357af467b656
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-04-29 14:08:51 +0500

    Added initial files

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -5,7 +5,26 @@ const Favorites = () => {
 
   return (
     <section className="favorites">
-      <div className=""></div>
+      <div className="favorites-content">
+        <h5>Favorites</h5>
+        <div className="favorites-container">
+          {favorites.map((item) => {
+            const { idMeal, strMealThumb: image } = item;
+
+            return (
+              <div key={idMeal} className="favorite-item">
+                <img src={image} className="favorites-img img" />
+                <button
+                  className="remove-btn"
+                  onClick={() => removeFromFavorites(idMeal)}
+                >
+                  remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
